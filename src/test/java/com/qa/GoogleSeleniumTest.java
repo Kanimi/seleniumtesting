01088768dbeecbbe3c5f095cde127b4ceb3ce9e5
Karolina <Karolina.Dudek@academytrainee.com>
2019-09-11 10:12:47 +0100

    added simple google image search test

--- src/test/java/com/qa/GoogleSeleniumTest.java
+++ src/test/java/com/qa/GoogleSeleniumTest.java
@@ -1,10 +1,47 @@
 package com.qa;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.Assert.assertTrue;
 
 public class GoogleSeleniumTest {
 
     private ChromeDriver driver;
 
+    @Before
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Admin\\Desktop\\seleniumtesting\\src\\test\\java\\resources\\chromedriver.exe");
+        driver = new ChromeDriver();
+    }
 
+    @After
+    public void tearDown(){
+        driver.close();
+    }
+
+    @Test
+    public void searchTest() throws InterruptedException {
+        driver.manage().window().maximize();
+        driver.get("http://google.com");
+        Thread.sleep(2000);
+        WebElement searchField = driver.findElement(By.name("q"));
+        assertTrue(searchField.isDisplayed());
+        searchField.sendKeys("marble fox pictures");
+        Thread.sleep(3000);
+        WebElement submitButton = driver.findElement(By.name("btnK"));
+        submitButton.click();
+        Thread.sleep(3000);
+        WebElement linkToBiggerPicture = driver.findElementByLinkText("Images for marble fox");
+        linkToBiggerPicture.click();
+        Thread.sleep(1000);
+        JavascriptExecutor jse = (JavascriptExecutor)driver;
+        jse.executeScript("window.scrollBy(0,500)");
+        Thread.sleep(5000);
+    }
 }
